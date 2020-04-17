cbae2b1e991e043cd27db477afa309c9b3e3168b
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-04-17 11:33:21 +0530

    adding the header component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container">
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {};
+import Header from './components/Header.vue';
+
+export default {
+  components: {
+    appHeader: Header,
+  },
+};
 </script>
 
 <style></style>
