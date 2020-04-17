26270b35b9323ebfd0ede21dfe2a5ce9eec5218c
Shankhadeep1234 <shankhadeepraj@gmail.com>
2020-04-17 12:41:50 +0530

    vuex

---
+++ src/store/modules/stocks.js
@@ -0,0 +1,16 @@
+const state = {
+  stocks: [],
+};
+
+const mutations = {
+  SET_STOCKS(state, stocks) {
+    state.stocks = stocks;
+  },
+  RND_STOCKS(state) {},
+};
+
+const actions = {
+  buyStock: ({ commit }, order) => {
+    commit();
+  },
+};
